f09b4fb1fb877cbf935310ab4fe377744c794f1f
Reagan <leoreagan@gmail.com>
2017-11-07 20:11:53 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+var port = 3000;
+
+app.set('view name','hbs');
+
+hbs.registerPartials(__dirname+"/views/partials");
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+app.use((req, res, next) => {
+  console.log(req.method);
+  console.log(req.url);
+  next();
+})
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    title: 'Home',
+    content:'Home page content'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up at ${port}`);
+});
